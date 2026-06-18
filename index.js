5ad4cd9364c7fc159d86a554cb48af3dc3533f35
Nilesh Shiradhonkar <nileshvshiradhonkar@gmail.com>
2026-06-18 20:16:43 +0530

    Update changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const db = require("./database");
 const fs = require("fs");
-const sqlite3 = require("sqlite3").verbose();
+// const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 app.use(bodyParser.json());
